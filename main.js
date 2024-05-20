0923673939b0f7cdda65830f1e0e6711d4be67e3
Awing <awingding.win@gmail.com>
2024-05-20 21:35:09 +0200

    retry on failure + split messages

--- main.js
+++ main.js
@@ -33,43 +33,53 @@ const jwt = new JWT({
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-async function getSheetClassementData(id){
+async function getSheetClassementData(id, tryCount = 0){
     return new Promise(async (resolve, reject) => {
-        const doc = new GoogleSpreadsheet(id, jwt);
-        await doc.loadInfo();
-        const sheet = doc.sheetsByIndex[0];
-        const sheet3 = doc.sheetsByIndex[2];
-        await sheet.loadCells("A1:CE121");
-        await sheet3.loadCells("C3");
-        let data = {};
-        let nom = sheet3.getCellByA1("C3").value;
-        data[nom] = {   "population": sheet.getCellByA1("F3").value,
-            "revenu": sheet.getCellByA1("J8").value,
-            "impots": sheet.getCellByA1("J3").value,
-            "production": sheet.getCellByA1("J4").value,
-            "exportation": sheet.getCellByA1("J6").value,
-            "infrastructure": sheet.getCellByA1("J5").value,
-            "income": sheet.getCellByA1("J16").value,
-            "armeetot": sheet.getCellByA1("J10").value,
-            "armee": sheet.getCellByA1("I32").value,
-            "marine": sheet.getCellByA1("I42").value,
-            "air": sheet.getCellByA1("I54").value,
-            "brut_dev": sheet.getCellByA1("U76").value,
-            "net_dev": sheet.getCellByA1("U79").value,
+        try {
+            const doc = new GoogleSpreadsheet(id, jwt);
+            await doc.loadInfo();
+            const sheet = doc.sheetsByIndex[0];
+            const sheet3 = doc.sheetsByIndex[2];
+            await sheet.loadCells("A1:CE121");
+            await sheet3.loadCells("C3");
+            let data = {};
+            let nom = sheet3.getCellByA1("C3").value;
+            data[nom] = {   
+                "population": sheet.getCellByA1("F3").value,
+                "revenu": sheet.getCellByA1("J8").value,
+                "impots": sheet.getCellByA1("J3").value,
+                "production": sheet.getCellByA1("J4").value,
+                "exportation": sheet.getCellByA1("J6").value,
+                "infrastructure": sheet.getCellByA1("J5").value,
+                "income": sheet.getCellByA1("J16").value,
+                "armeetot": sheet.getCellByA1("J10").value,
+                "armee": sheet.getCellByA1("I32").value,
+                "marine": sheet.getCellByA1("I42").value,
+                "air": sheet.getCellByA1("I54").value,
+                "brut_dev": sheet.getCellByA1("U76").value,
+                "net_dev": sheet.getCellByA1("U79").value,
 
-            "bois": sheet.getCellByA1("L60").value,
-            "pierre": sheet.getCellByA1("L61").value,
-            "plante": sheet.getCellByA1("L62").value,
-            "sable": sheet.getCellByA1("L63").value,
-            "alu": sheet.getCellByA1("L64").value,
-            "carbone": sheet.getCellByA1("L65").value,
-            "chromium": sheet.getCellByA1("L66").value,
-            "cuivre": sheet.getCellByA1("L67").value,
-            "fer": sheet.getCellByA1("L68").value,
-            "charbon": sheet.getCellByA1("L69").value,
-            "gaz": sheet.getCellByA1("L70").value,
-            "petrole": sheet.getCellByA1("L71").value
-        };
+                "bois": sheet.getCellByA1("L60").value,
+                "pierre": sheet.getCellByA1("L61").value,
+                "plante": sheet.getCellByA1("L62").value,
+                "sable": sheet.getCellByA1("L63").value,
+                "alu": sheet.getCellByA1("L64").value,
+                "carbone": sheet.getCellByA1("L65").value,
+                "chromium": sheet.getCellByA1("L66").value,
+                "cuivre": sheet.getCellByA1("L67").value,
+                "fer": sheet.getCellByA1("L68").value,
+                "charbon": sheet.getCellByA1("L69").value,
+                "gaz": sheet.getCellByA1("L70").value,
+                "petrole": sheet.getCellByA1("L71").value
+            };
+        } catch (error) {
+            if (tryCount > 3) {
+                reject(error);
+            }
+                setTimeout(() => {
+                    getSheetClassementData(id, tryCount + 1).then((data) => {resolve(data)});
+                }, 5000);
+        }
         resolve(data);
     });
 }
@@ -111,17 +121,21 @@ async function sendClassement(channel){
 async function outputClassement(data, variable, name, channel){
     data.sort((a, b) => {
         return a[Object.keys(a)[0]][variable] - b[Object.keys(b)[0]][variable];
-    });
-    let message = `**Classement des pays par ${name} :**\n`;
+    }).reverse();
+    let message = [`**Classement des pays par ${name} :**\n`];
     let country = "";
     for (let obj in data){
         country = Object.keys(data[obj])[0];
-        message += `${country} : ${round(data[obj][country][variable], 2)}\n`;
+        message.push(`${country} : ${round(data[obj][country][variable], 2)}\n`);
     }
-    client.channels.fetch(channel).then(channel => {channel.send(message)});
+    let msgArrayLength = message.length;
+    let msg1 = message.slice(0, Math.ceil(msgArrayLength / 2)).join("");
+    let msg2 = message.slice(Math.ceil(msgArrayLength / 2)).join("");
+    client.channels.fetch(channel).then(channel => {
+        channel.send(msg1);
+        channel.send(msg2);
+    });
 }
-
-
 
     function round(value, decimals) {
         return Number(value.toPrecision(decimals));
